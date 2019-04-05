b35a482838de42b0a76e23255c407f11f97d9963
AJLambda <apj2277@gmail.com>
2019-04-05 09:32:59 -0700

    created AXIOS request with GET to display smurfs

--- village/src/App.js
+++ village/src/App.js
@@ -1,16 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
+
+  // Construct an AXIOS request to retrieve an array all the Smurfs in the Smurf DB simply write a GET to the endpoint /smurfs.
+  componentDidMount() {
+    // ajax request to get the items from a server on mount
+    // 1. invoke .get()
+    // 2. pass in the server url - base url + endpoint
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(res => {
+        this.setState({ smurfs: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
