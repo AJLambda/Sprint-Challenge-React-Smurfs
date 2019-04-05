33fd29286e073e70fca702212f54203cea0a7e18
AJLambda <apj2277@gmail.com>
2019-04-05 09:35:49 -0700

    created AXIOS request with POST to add a new Smurf

--- village/src/App.js
+++ village/src/App.js
@@ -28,6 +28,20 @@ class App extends Component {
       });
   }
 
+  //Construct an AXIOS request to POST to add a Smurf to the Smurf DB you'll need all three fields.
+  addNewSmurf = newSmurf => {
+    axios
+      .post("http://localhost:3333/smurfs", newSmurf)
+      .then(res => {
+        this.setState({
+          smurfs: res.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
