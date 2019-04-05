d25f24ff896f34c39db749a66c584c971311d598
AJLambda <apj2277@gmail.com>
2019-04-05 09:49:01 -0700

    created NavLinks to route my pages

--- village/src/App.js
+++ village/src/App.js
@@ -47,9 +47,25 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  // Create two `routes` in your `App` component, one at `'/'` for your `Smurfs` component,and one at `/smurf-form` for your form.
+
+  //Then make it so you can navigate to your routes using `Link`.
+
+  //Then in your `App` component, create a nav bar that will use `NavLink` to route to your different pages.
+
   render() {
     return (
       <div className="App">
+        <div className="navBar">
+          <NavLink exact to="/" className="activeNav">
+            Smurfs
+          </NavLink>
+          <NavLink to="/smurf-form" className="activeNav">
+            Smurf Form
+          </NavLink>
+        </div>
+
         <Route
           exact
           path="/"
